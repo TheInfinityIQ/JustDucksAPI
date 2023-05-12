c38a532cd751384648ff649c302a677a25682189
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2023-05-12 18:55:10 -0500

    add cors in development

--- JustDucks.Api/Program.cs
+++ JustDucks.Api/Program.cs
@@ -1,5 +1,22 @@
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(c =>
+{
+    var corsPolicy = new CorsPolicy();
+    builder.Configuration.GetRequiredSection(nameof(CorsPolicy)).Bind(corsPolicy);
+    c.AddDefaultPolicy(corsPolicy);
+});
+
+//TODO: AddAuthentication
+//TODO: AddAuthorization
+
 var app = builder.Build();
+
+app.UseCors();
+
+//TODO: UseAuthentication
+//TODO: UseAuthorization
 
 app.MapGet("/", () => "Hello World!");
 
